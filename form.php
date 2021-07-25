ae9a60236989cfa01605013bedc4a5c95f847f5b
ruchi083 <ruchipanse143@gmail.com>
2021-07-25 20:05:41 +0530

    Immune up's First Commit

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>form</title>
+  </head>
+  <body>
+<form action="reg.php" method="post">
+
+  <label for="name">First name:</label><br>
+  <input type="text"  name="name"><br>
+  <label for="text">aadharno</label><br>
+  <input type="text"  name="text"><br>
+  <label for="Gender">gender</label><br>
+  <input type="text"  name="Gender"><br>
+  <label for="pass">password</label><br>
+  <input type="password"  name="pass">
+  <input type="submit" name="submit" value="Submit">
+
+</form>
+  </body>
+</html>
